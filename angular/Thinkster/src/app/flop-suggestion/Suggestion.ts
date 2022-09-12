bf5e5b73b9efabbffec1202e7b17e942ad46a918
Maz Qureshi <muhammadmaaz40@gmail.com>
2022-09-12 16:46:00 -0500

    ng other form components

---
+++ angular/Thinkster/src/app/flop-suggestion/Suggestion.ts
@@ -0,0 +1,9 @@
+export class Suggestion {
+    id: number = 0;
+    title: string='';
+    year: number=0;
+    stars: number=0;
+    rating: string='';
+    studio: string='';
+    hasSoundtrack: boolean=false;
+  }
